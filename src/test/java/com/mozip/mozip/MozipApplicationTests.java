0471fcdb429436384214e212350d4be571c5632b
정재웅 <jweidon0821@gmail.com>
2024-09-15 07:28:09 +0900

    [INIT] 프로젝트 세팅

---
+++ src/test/java/com/mozip/mozip/MozipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozip.mozip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
