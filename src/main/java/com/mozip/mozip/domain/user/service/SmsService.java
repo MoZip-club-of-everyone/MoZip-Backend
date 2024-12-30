1bbd0898c74715b90ab2577b45abcb4f01dcb0d9
leestana01 <leestana01@naver.com>
2024-12-30 18:00:43 +0900

    feat(SmsService): #19 휴대폰 인증 구현

---
+++ src/main/java/com/mozip/mozip/domain/user/service/SmsService.java
@@ -0,0 +1,62 @@
+package com.mozip.mozip.domain.user.service;
+
+import com.mozip.mozip.domain.user.entity.VerificationCode;
+import com.mozip.mozip.domain.user.repository.VerificationCodeRepository;
+import lombok.RequiredArgsConstructor;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+@Service
+@RequiredArgsConstructor
+public class SmsService {
+    private final VerificationCodeRepository verificationCodeRepository;
+
+    public boolean sendVerificationCode(String phone) {
+        String verificationCode = String.valueOf((int) (Math.random() * 900000) + 100000);
+        String message = "인증번호는 [" + verificationCode + "] 입니다.";
+        boolean isSent = sendSms(phone, message);
+
+        if (isSent) {
+            VerificationCode entity = new VerificationCode();
+            entity.setPhone(phone);
+            entity.setCode(verificationCode);
+            entity.setCreatedAt(LocalDateTime.now());
+            entity.setExpiresAt(LocalDateTime.now().plusMinutes(5)); // 5분
+
+            verificationCodeRepository.save(entity);
+        } else {
+            System.out.println("인증코드 생성에 실패하였습니다.");
+        }
+
+        return isSent;
+    }
+
+    public boolean verifyCode(String phone, String code) {
+        return verificationCodeRepository.findByPhoneAndCode(phone, code)
+                .filter(v -> v.getExpiresAt().isAfter(LocalDateTime.now()))
+                .isPresent();
+    }
+
+    private boolean sendSms(String phone, String message) {
+        Message coolsms = new Message("NCSIAFJIB2WSB3D2", "BRTAFGT1OKTYBSYISLQ6NTOPV9AY5MBV");
+        HashMap<String, String> params = new HashMap<>();
+        params.put("to", phone);
+        params.put("from", "01040694033");
+        params.put("type", "SMS");
+        params.put("text", message);
+
+        try {
+            JSONObject response = coolsms.send(params);
+            System.out.println("SMS sent successfully: " + response.toJSONString());
+            return true;
+        } catch (CoolsmsException e) {
+            System.out.println("Failed to send SMS: " + e.getCode() + " - " + e.getMessage());
+            return false;
+        }
+    }
+}
